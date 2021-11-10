2935117845c22e16f55834c58bdae4fc1e6247cc
Byron Jimenez <byronjimenez9911@gmail.com>
2021-11-10 07:11:55 -0600

    Login form

---
+++ Proyecto final/Login.Designer.cs
@@ -0,0 +1,120 @@
+﻿
+namespace Proyecto_final
+{
+    partial class Login
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.label1 = new System.Windows.Forms.Label();
+            this.label2 = new System.Windows.Forms.Label();
+            this.label3 = new System.Windows.Forms.Label();
+            this.TxtUsuario = new System.Windows.Forms.TextBox();
+            this.TxtPass = new System.Windows.Forms.TextBox();
+            this.button1 = new System.Windows.Forms.Button();
+            this.SuspendLayout();
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Font = new System.Drawing.Font("Poppins", 15F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label1.Location = new System.Drawing.Point(26, 22);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(205, 44);
+            this.label1.TabIndex = 0;
+            this.label1.Text = "Por siempre 30";
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Location = new System.Drawing.Point(75, 103);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(61, 17);
+            this.label2.TabIndex = 1;
+            this.label2.Text = "Usuario:";
+            // 
+            // label3
+            // 
+            this.label3.AutoSize = true;
+            this.label3.Location = new System.Drawing.Point(53, 164);
+            this.label3.Name = "label3";
+            this.label3.Size = new System.Drawing.Size(85, 17);
+            this.label3.TabIndex = 2;
+            this.label3.Text = "Contraseña:";
+            // 
+            // TxtUsuario
+            // 
+            this.TxtUsuario.Location = new System.Drawing.Point(164, 100);
+            this.TxtUsuario.Name = "TxtUsuario";
+            this.TxtUsuario.Size = new System.Drawing.Size(300, 22);
+            this.TxtUsuario.TabIndex = 3;
+            // 
+            // TxtPass
+            // 
+            this.TxtPass.Location = new System.Drawing.Point(164, 160);
+            this.TxtPass.Name = "TxtPass";
+            this.TxtPass.PasswordChar = '*';
+            this.TxtPass.Size = new System.Drawing.Size(300, 22);
+            this.TxtPass.TabIndex = 4;
+            // 
+            // button1
+            // 
+            this.button1.Location = new System.Drawing.Point(164, 215);
+            this.button1.Name = "button1";
+            this.button1.Size = new System.Drawing.Size(135, 37);
+            this.button1.TabIndex = 5;
+            this.button1.Text = "Iniciar sesión";
+            this.button1.UseVisualStyleBackColor = true;
+            this.button1.Click += new System.EventHandler(this.button1_Click);
+            // 
+            // Login
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 16F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(703, 317);
+            this.Controls.Add(this.button1);
+            this.Controls.Add(this.TxtPass);
+            this.Controls.Add(this.TxtUsuario);
+            this.Controls.Add(this.label3);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.label1);
+            this.Name = "Login";
+            this.Text = "Login";
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.Label label3;
+        private System.Windows.Forms.TextBox TxtUsuario;
+        private System.Windows.Forms.TextBox TxtPass;
+        private System.Windows.Forms.Button button1;
+    }
+}
---
+++ Proyecto final/Login.cs
@@ -0,0 +1,67 @@
+﻿using MySql.Data.MySqlClient;
+using Proyecto_final.Helpers;
+using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace Proyecto_final
+{
+    public partial class Login : Form
+    {
+        private Connection connection;
+        public Login()
+        {
+            InitializeComponent();
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            try
+            {
+                string usuario = TxtUsuario.Text;
+                string password = TxtPass.Text;
+
+                if (string.IsNullOrEmpty(usuario) || string.IsNullOrEmpty(password))
+                {
+                    MessageBox.Show("Ingrese usuario y contraseña");
+                }
+                else
+                {
+                    connection = new Connection();
+                    using (var conn = connection.openConnection())
+                    {
+                        MySqlCommand cmd = new MySqlCommand();
+                        cmd.Connection = conn;
+                        cmd.CommandText = "select * from usuarios where (username=@user and password=@pass)";
+                        cmd.Parameters.AddWithValue("@user", usuario);
+                        cmd.Parameters.AddWithValue("@pass", password);
+                        cmd.CommandType = CommandType.Text;
+                        MySqlDataReader reader = cmd.ExecuteReader();
+                        if (reader.HasRows)
+                        {
+                            var dashboard = new Form1();
+                            dashboard.Show();
+                            this.Hide();
+                        }
+                        else
+                        {
+                            MessageBox.Show("Usuario incorrecto");
+                        }
+
+                    }
+                }
+            }
+            catch (Exception)
+            {
+
+                MessageBox.Show("Ocurrió un problema, verifica los datos");
+            }
+        }
+    }
+}
